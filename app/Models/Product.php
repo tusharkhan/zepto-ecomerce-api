8ae0dc03f2b89c8111d338e90dd28f646dafcd1a
tushar <tushar.khan0122@gmail.com>
2022-04-03 06:21:21 +0600

    product image

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+
+    public function getImageAttribute()
+    {
+        return asset('storage/products/' .  $this->attributes['image']);
+    }
 }
